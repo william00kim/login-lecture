cb07d4531197556f5acf70341448409d08c0a074
william00kim <rlawhdgus1212@gmail.com>
2024-11-21 18:42:28 +0900

    asdasd

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,14 @@
-"use strict"
+const id = document.getElementById('id');
+const password = document.getElementById("password");
+const login_btn = document.getElementById("login_btn");
 
-console.log("hello world");
+login_btn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id : id.value,
+        password : password.value
+    }
+
+    console.log(req);
+}
