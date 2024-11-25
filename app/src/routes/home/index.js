b06ba539ae3757157834af2b90046fc1d1eb4b65
william00kim <rlawhdgus1212@gmail.com>
2024-11-25 22:18:56 +0900

    프론트엔드 로그인 기능 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,6 +7,6 @@ const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.home); 
 router.get("/login", ctrl.output.login);
-router.post("/login", ctrl.process.login);
+// router.post("/login", ctrl.process.login);
 
 module.exports = router;
